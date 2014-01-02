90a864180b5b77496ab3b1fbe0dee8cf071188e0
coybit <coybit@gmail.com>
2014-01-02 13:05:17 +0330

    extacting more data from cloc output

--- main.js
+++ main.js
@@ -4,48 +4,97 @@ var childProcess = require('child_process');
 var clocPath = '~/WebstormProjects/SrcStatJS/SrcStatJS/cloc-1.60.pl';
 var gitScorePath = '~/WebstormProjects/SrcStatJS/SrcStatJS/git-score.py';
 var projectsPath = [
-
     {name:'project1',
-        path: '~/Documents/herokuprojects/project2'},
+     path: '~/Documents/herokuprojects/project1'},
 
     {name:'project2',
-        path: '~/Documents/xcodeprojects/project1'}
+     path: '~/Documents/xcodeprojects/project2'}
 ];
+var debugLevel = 2;
 
 
+function dlog(msg,level) {
 
-function puts(error, stdout, stderr) {
+    if(level<=debugLevel)
+        sys.puts(msg);
+}
 
-    if(error)
-        return sys.puts(error);
+function gitScoreOutputAnalyzerfunction(err,stdout,stderr){
 
+    if(err)
+        return dlog(err,1);
 
-    stdout.match(/SUM\:.+/g).forEach(function(m){
-        var numbers =  m.match(/\d+/g);
+    var users = [];
+    var lines = stdout.split('\n')
+    lines.shift();  // Remove first line
+    lines.pop();    // Remove last line
 
-        var file = parseInt(numbers[0]);
-        var empty = parseInt(numbers[1]);
-        var comment = parseInt(numbers[2]);
-        var code = parseInt(numbers[3]);
+    lines.forEach(function(line) {
+        var user = line.split('\t')
+        users.push( {
+            name:user[0],
+            commit:user[1],
+            files:user[2],
+            delta:user[3],
+            plus:user[4],
+            minus:user[5]
+        });
+    })
 
-        console.log(  empty+comment+code )
+    users.forEach(function(user){
+        dlog(user.name + ' ... ' + user.commit , 2);
     })
 }
 
+function clocOutputAnalyzer(error, stdout, stderr) {
+
+    if(error)
+        return dlog(error,1);
+
+    dlog(stdout,3);
+
+    var languages = [];
+
+    stdout.split('\n').slice(8).forEach(function(line){
+
+        if(line==undefined || line.length==0 || line.indexOf('---')!=-1)
+            return;
+
+        // Valid Line Format: [Language] [Files] [Blank] [Comment] [Code][EOL]
+
+        var parts = line.match(/((\w|\+|\/|\:)+(\s|$))+/g);
+        var language = {
+            name: parts[0],
+            files: parseInt(parts[1]),
+            blank: parseInt(parts[2]),
+            comment: parseInt(parts[3]),
+            code: parseInt(parts[4])
+        }
+
+        languages.push(language);
+
+        dlog(language.name + '...' + (language.code+language.comment+language.blank), 2);
+    });
+}
+
+/*
+Extracting information about the used languages and LOC for each one
+ */
 function cloc() {
     for( var i=0; i<projectsPath.length; i++ ){
 
-        var glog = function(p) {
+        childProcess.exec('perl ' + clocPath + ' ' + projectsPath[i].path, (function(project){
             return (function(error,stdout,stderr) {
-                sys.puts(p.name + ' at ' + p.path)
-                puts(error,stdout,stderr);
+                dlog(project.name + ' at ' + project.path,2)
+                clocOutputAnalyzer(error,stdout,stderr);
             });
-        }
-
-        childProcess.exec('perl ' + clocPath + ' ' + projectsPath[i].path, glog(projectsPath[i]));
+        }) (projectsPath[i]));
     }
 }
 
+/*
+Extracting information about user contribution in each repository
+ */
 function gitscore() {
 
     for( var i=0; i<projectsPath.length; i++) {
@@ -53,37 +102,10 @@ function gitscore() {
         wd = wd.replace('~','/Users/coybit/');
 
         childProcess.exec('python ' + gitScorePath, {cwd:wd}, (function(project){
-
-                return function(err,stdout,stderr){
-
-                sys.puts(project.name);
-
-                if(err)
-                    return sys.puts(err);
-
-                var users = [];
-                var lines = stdout.split('\n')
-                lines.shift();  // Remove first line
-                lines.pop();    // Remove last line
-
-                lines.forEach(function(line) {
-                    var user = line.split('\t')
-                    users.push( {
-                        name:user[0],
-                        commit:user[1],
-                        files:user[2],
-                        delta:user[3],
-                        plus:user[4],
-                        minus:user[5]
-                    });
-                })
-
-                sys.puts('.......................');
-                users.forEach(function(user){
-                    sys.puts(user.name + ' ... ' + user.commit );
-                })
-            };
-
+            return (function(error,stdout,stderr) {
+                dlog(project.name + ' at ' + project.path,2)
+                gitScoreOutputAnalyzerfunction(error,stdout,stderr);
+            });
         })(projectsPath[i]));
     }
 }
